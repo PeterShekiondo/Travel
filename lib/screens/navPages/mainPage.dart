b294d141c3e8cb3deb23ff09cef2ef123d0f6766
peter shekiondo <peter@databalk.africa>
2021-12-21 00:02:07 +0300

    refine home page

--- lib/screens/navPages/mainPage.dart
+++ lib/screens/navPages/mainPage.dart
@@ -24,14 +24,13 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(Icons.menu, size: 30, color:Colors.black45),
+                Icon(Icons.menu, size: 30, color: Colors.black45),
                 Container(
                   width: 40,
                   height: 40,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(.5)
-                  ),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(.5)),
                 )
               ],
             ),
@@ -40,12 +39,12 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
           // Discover text
           Container(
               padding: const EdgeInsets.only(left: 20, right: 20),
-              child: AppText(text: 'Discover',
-                  size: 25,
-                  color: AppColors.bigTextColor,
-                  weight: FontWeight.bold,
-              )
-          ),
+              child: AppText(
+                text: 'Discover',
+                size: 25,
+                color: AppColors.bigTextColor,
+                weight: FontWeight.bold,
+              )),
           SizedBox(height: 25),
           // Tab bar
           Container(
@@ -58,26 +57,25 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                 isScrollable: true,
                 labelPadding: const EdgeInsets.only(left: 20, right: 20),
                 indicatorSize: TabBarIndicatorSize.label,
-                indicator: CircleTabIndicator(color: AppColors.mainColor, radius: 4),
+                indicator:
+                    CircleTabIndicator(color: AppColors.mainColor, radius: 4),
                 tabs: [
                   Container(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Places",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    )
-                  ),
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Places",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      )),
                   Container(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Inspiration",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    )
-                  ),
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        "Inspiration",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      )),
                   Container(
                       padding: const EdgeInsets.only(bottom: 10),
                       child: Text(
@@ -85,8 +83,7 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                         style: TextStyle(
                           fontSize: 16,
                         ),
-                      )
-                  ),
+                      )),
                 ],
               ),
             ),
@@ -97,32 +94,86 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
             child: TabBarView(
               controller: _tabController,
               children: [
-              ListView.builder(
-                padding: const EdgeInsets.only(left: 20, top: 5),
-                itemCount : 3,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 20, top: 15),
-                    width: 200,
-                    height: 300,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                              "images/mountain.jpeg"
-                          ),
-                        )
-                    ),
-                  );
-                },
-              ),
+                ListView.builder(
+                  padding: const EdgeInsets.only(left: 20, top: 5),
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 20, top: 15),
+                      width: 200,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("images/mountain.jpeg"),
+                          )),
+                    );
+                  },
+                ),
                 Text("welcome"),
                 Text("bye")
               ],
             ),
+          ),
+          SizedBox(height: 25),
+          Container(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppText(
+                  text: 'Explore more',
+                  size: 18,
+                  color: AppColors.bigTextColor,
+                  weight: FontWeight.bold,
+                ),
+                AppText(
+                  text: 'See all',
+                  size: 18,
+                  color: AppColors.textColor1,
+                  weight: FontWeight.bold,
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 25),
+          Container(
+            height:120,
+            width: double.maxFinite,
+            child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 90,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage("images/mountain.jpeg"),
+                              )),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: AppText(
+                            text: "Kayaking",
+                            size: 14,
+                          )
+                        )
+                      ],
+                    ),
+                  );
+                }),
           )
         ],
       ),
@@ -130,26 +181,32 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   }
 }
 
-class CircleTabIndicator extends Decoration{
+class CircleTabIndicator extends Decoration {
   final Color color;
   double radius;
+
   CircleTabIndicator({required this.color, required this.radius});
+
   @override
   BoxPainter createBoxPainter([VoidCallback? onChanged]) {
-    return _CirclePainter(color:color, radius:radius);
+    return _CirclePainter(color: color, radius: radius);
   }
 }
 
-class _CirclePainter extends BoxPainter{
+class _CirclePainter extends BoxPainter {
   final Color color;
   double radius;
+
   _CirclePainter({required this.color, required this.radius});
+
   @override
   void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
     Paint _paint = Paint();
     _paint.color = color;
     _paint.isAntiAlias = true;
-    final Offset circleOffset = Offset(configuration.size!.width/2 - radius/2, configuration.size!.height - radius);
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
     canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
